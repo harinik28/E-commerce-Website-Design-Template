6fff8659a45d8cfe711ea580f079821aab540727
Harini K <harinik2805@gmail.com>
2024-09-28 21:10:21 +0530

    Delete Footer.jsx

--- Footer.jsx
+++
@@ -1,8 +0,0 @@
-const Footer=()=>{
-    return(
-        <div className="d-flex justify-content-center align-items-center footer">
-            <h4>React Js is Javascript library</h4>
-        </div>
-    )
-}
-export default Footer

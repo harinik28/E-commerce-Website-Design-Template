76e91f61ba805ee93143bfaaaeaaa8f29bcf85ef
Harini K <harinik2805@gmail.com>
2024-10-08 17:33:51 +0530

    Delete src/Product.jsx

--- src/Product.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Product = () => {
-  return (
-    <h1 className=' vh-100 d-flex align-items-center justify-content-center mb-0'>Product</h1>
-  )
-}
-
-export default Product
